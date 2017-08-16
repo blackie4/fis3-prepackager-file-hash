7190c36e99574fc4bf1ba7ceb3116ea594ed2cfa
mingyi.ymy <mingyi.ymy@alibaba-inc.com>
2017-08-16 15:09:43 +0800

    feat: Remove log

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ module.exports = function (ret, pack, settings, opt) {
     if (content.replace) {
       // 指定文件hash
       content = content.replace(reg1, function (str, quote, filePath) {
-        console.log(filePath)
         var resFile = getMapFile(ret, filePath);
         if (!resFile) return str;
         return quote + resFile.getHash() + quote;
